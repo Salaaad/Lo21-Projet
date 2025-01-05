409aa7b3ada78f980df1c6d00ec2b44b7a7b6673
Salaaad <135101680+Salaaad@users.noreply.github.com>
2025-01-05 16:22:00 +0100

    FINAL

--- main.c
+++ main.c
@@ -3,36 +3,57 @@
 #include "reseau.h"
 
 int main() {
-    // // Créer une couche avec 3 neurones, chacun ayant 2 entrées                                                  TEST AVEC JUSTE LES COUCHES
-    // Couche* maCouche = InitCouche(2, 1);
-
-    // // Créer une liste chaînée d'entrées
-    // Entrée* mesEntrees = creerListeEntrees(1); // Deux entrées, e.g., {1, 0}
-
-    // // Calculer les sorties de la couche
-    // Entrée* sorties = Outcouche(maCouche, mesEntrees);
-
-    // // Afficher les sorties
-    // printf("Sorties de la couche : ");
-    // while (sorties != NULL) {
-    //     printf("%d ", sorties->valeur);
-    //     sorties = sorties->suivant;
-    // }
-    // printf("\n");
 
 
 
 
 
+    /** 
+    // Créer une couche avec 3 neurones, chacun ayant 2 entrées                                                  TEST AVEC JUSTE LES COUCHES
+    Couche* maCouche = InitCouche(2, 1);
 
-    // Neurone* Node=InitNeur(2);                           TEST AVEC JUSTE LES NEURONES
-    // Entrée* entry=creerListeEntrees(2);
-    // afficherNeurone(Node);
-    // printf("\n%i",Outneurone(Node,entry));
+    // Créer une liste chaînée d'entrées
+    Entrée* mesEntrees = creerListeEntrees(1); // Deux entrées, e.g., {1, 0}
+
+    // Calculer les sorties de la couche
+    Entrée* sorties = Outcouche(maCouche, mesEntrees);
+
+    // Afficher les sorties
+    printf("Sorties de la couche : ");
+    while (sorties != NULL) {
+        printf("%d ", sorties->valeur);
+        sorties = sorties->suivant;
+    }
+    printf("\n");
+
+    */
 
 
 
-    int nbCouches = 2;  
+
+
+
+
+
+
+   /** 
+
+    Neurone* Node=InitNeur(2);                           //TEST AVEC JUSTE LES NEURONES
+    Entrée* entry=creerListeEntrees(2);
+    afficherNeurone(Node);
+    printf("\n%i",Outneurone(Node,entry));
+
+
+    */
+
+
+
+
+
+   /** 
+
+
+    int nbCouches = 2;                                       // RESEAU ENTIER
     int neuronesParCouche[] = {3,2};  
     int entreesPremiereCouche = 3;  
 
@@ -47,6 +68,129 @@ int main() {
     Entrée* sortieReseau = OutReseau(reseau, entreesInitiales);
     printf("Sortie du réseau : ");
     afficherListeEntrees(sortieReseau);
+
+
+    */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   /** 
+
+
+
+
+    int nbCouches = 1;                                   // RESEAU ET
+    int neuronesParCouche[] = {1};  
+    int entreesPremiereCouche = 3;  
+
+    Reseau* reseau = CreerResNeur(nbCouches, neuronesParCouche, entreesPremiereCouche);
+
+    afficherReseau(reseau);
+
+    Entrée* entreesInitiales = creerListeEntrees(entreesPremiereCouche);  
+    printf("Entrée du réseau : ");
+    afficherListeEntrees(entreesInitiales);
+
+    Entrée* sortieReseau = OutReseau(reseau, entreesInitiales);
+    printf("Sortie du réseau : ");
+    afficherListeEntrees(sortieReseau);
         
+    
+    */  
+
+
+
+
+
+
+
+   /** 
+
+
+    int nbCouches = 1;                                   // RESEAU OU
+    int neuronesParCouche[] = {1};  
+    int entreesPremiereCouche = 3;  
+
+    Reseau* reseau = CreerResNeur(nbCouches, neuronesParCouche, entreesPremiereCouche);
+
+    afficherReseau(reseau);
+
+    Entrée* entreesInitiales = creerListeEntrees(entreesPremiereCouche);  
+    printf("Entrée du réseau : ");
+    afficherListeEntrees(entreesInitiales);
+
+    Entrée* sortieReseau = OutReseau(reseau, entreesInitiales);
+    printf("Sortie du réseau : ");
+    afficherListeEntrees(sortieReseau);
+        
+    */
+
+   
+
+
+
+
+    /** 
+
+    int nbCouches = 1;                                   // RESEAU NOT
+    int neuronesParCouche[] = {1};  
+    int entreesPremiereCouche = 1;  
+
+    Reseau* reseau = CreerResNeur(nbCouches, neuronesParCouche, entreesPremiereCouche);
+
+    afficherReseau(reseau);
+
+    Entrée* entreesInitiales = creerListeEntrees(entreesPremiereCouche);  
+    printf("Entrée du réseau : ");
+    afficherListeEntrees(entreesInitiales);
+
+    Entrée* sortieReseau = OutReseau(reseau, entreesInitiales);
+    printf("Sortie du réseau : ");
+    afficherListeEntrees(sortieReseau);
+        
+
+    */
+
+
+
+
+    int nbCouches = 3;                                   // RESEAU MULTI COUCHE [A ET (NON B) ET C] OU [A ET (NON C)]
+    int neuronesParCouche[] = {5,2,1};  
+    int entreesPremiereCouche = 3;  
+
+    Reseau* reseau = CreerResNeur(nbCouches, neuronesParCouche, entreesPremiereCouche);
+
+    afficherReseau(reseau);
+
+    Entrée* entreesInitiales = creerListeEntrees(entreesPremiereCouche);  
+    printf("Entrée du réseau : ");
+    afficherListeEntrees(entreesInitiales);
+
+    Entrée* sortieReseau = OutReseau(reseau, entreesInitiales);
+    printf("Sortie du réseau : ");
+    afficherListeEntrees(sortieReseau);
+        
+
+
+
+
+
+
     return 0;
 }
